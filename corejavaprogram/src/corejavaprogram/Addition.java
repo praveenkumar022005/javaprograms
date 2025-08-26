7433b96c9257b1f7bb300343a201c79ca3c5fd55
praveenkumar022005 <praveenpondy924@gmail.com>
2025-08-26 10:59:49 +0530

    Update Addition.java

--- corejavaprogram/src/corejavaprogram/Addition.java
+++ corejavaprogram/src/corejavaprogram/Addition.java
@@ -3,7 +3,20 @@ package corejavaprogram;
 public class Addition {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+
+        
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+        
+        int sum = num1 + num2;
+
+        
+        System.out.println("The sum is: " + sum);
 
 	}
 
